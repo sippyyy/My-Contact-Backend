3e5a59974709a5924a46301243f7a5cb75465c6b
sippyyy <nguyenthuy030800@gmail.com>
2023-05-12 00:01:37 +0700

    update access token and add middleware validateToken

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require( 'express' )
 const {loginUser,registerUser,currentUser} = require('../controllers/userController');
 const router = express.Router()
+const validateToken = require('../middleware/validateTokenHandler')
 
 router.route( '/login').post(loginUser);
 
 router.route( '/register').post(registerUser);
 
-router.route( '/current' ).post(currentUser);
+router.route( '/current' ).get(validateToken,currentUser);
 
 
 module.exports = router
